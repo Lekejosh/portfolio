539136655d2d43749aeed8d6da9091747126867e
lekejosh <lekejosh6wf@gmail.com>
2022-04-07 22:05:10 +0100

    Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./components/Home";
 import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaTwitter } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo.png";
 function Navbar() {
   const [nav, setNav] = useState(false);
@@ -43,7 +45,37 @@ function Navbar() {
         </ul>
       </div>
       {/*Social Icons*/}
-      <div className="hidden"> </div>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-black">
+            <a
+              className="flex justify-between items-center w-full text-grey-300"
+              href="https://github.com/lekejosh"
+              target="blank"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-red-400">
+            <a
+              className="flex justify-between items-center w-full text-grey-300"
+              href="mailto:lekejosh6wf@gmail.com"
+              target="blank"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-400">
+            <a
+              className="flex justify-between items-center w-full text-grey-300"
+              href="https://twitter.com/joshuadeleke"
+              target="blank"
+            >
+              Twitter <FaTwitter size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
