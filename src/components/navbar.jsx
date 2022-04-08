ae31d1ac17f9f5b19b13b58d88bf7b936f602870
lekejosh <lekejosh6wf@gmail.com>
2022-04-08 22:04:43 +0100

    removed an icon

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import { FaBars, FaTimes, FaGithub, FaTwitter } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-import { BsFillPersonLinesFill } from "react-icons/bs";
+
 import Logo from "../assets/logo.png";
 import { Link } from "react-scroll";
 function Navbar() {
