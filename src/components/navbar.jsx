36eae551527d92b6379cdc47354e187d49a6f339
lekejosh <lekejosh6wf@gmail.com>
2022-04-08 20:56:46 +0100

    logo change

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,11 +11,9 @@ function Navbar() {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <a href="#Home">
-          <h2 className="text-grey-300 text-xl" style={{ width: "50px" }}>
-            LekeJosh
-          </h2>
-        </a>
+        <Link to="Home" smooth={true} duration={500}>
+          <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+        </Link>
       </div>
       {/*Menu*/}
       <ul className="hidden md:flex">
