ac77d7b17d3b112098c12f5b4ff7c41227fbc40b
lekejosh <lekejosh6wf@gmail.com>
2022-08-16 17:52:25 +0100

    removed some sites

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -17,7 +17,7 @@ const Works = () => {
           <p className="py-6"> // Check out some of my recent work</p>
         </div>
         {/*Container*/}
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/*Grid Item*/}
           <div
             style={{ backgroundImage: `url(${Bench})` }}
@@ -106,87 +106,6 @@ const Works = () => {
                 </a>
                 <a
                   href="https://github.com/Lekejosh/weather-app"
-                  target="_blank"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-gray-400 text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${crowd})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/*Hover Effects*/}
-            <div className="opacity-0 group-hover:opacity-80">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Crowdfunding
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://fm-crowdfunding.vercel.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-gray-400 text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/Lekejosh/Crowdfunding"
-                  target="_blank"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-gray-400 text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${calc})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/*Hover Effects*/}
-            <div className="opacity-0 group-hover:opacity-80">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Calculator
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://leke-calc.netlify.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-gray-400 text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/Lekejosh/calculator-app"
-                  target="_blank"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-gray-400 text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${auto})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/*Hover Effects*/}
-            <div className="opacity-0 group-hover:opacity-80">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Auuto Location Weather Detector
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://lekejosh.github.io/Auto-Locate-Weather-App/"
-                  target="_blank"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-gray-400 text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://lekejosh.github.io/Auto-Locate-Weather-App/"
                   target="_blank"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-gray-400 text-gray-700 font-bold text-lg">
